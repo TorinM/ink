d4cc04642e7c99519d9259dda2b3d8a119e7b88a
TorinM <torinmay@gmail.com>
2025-09-29 17:23:47 -0500

    Added insert function

--- ink/src/gap_buffer/mod.rs
+++ ink/src/gap_buffer/mod.rs
@@ -4,27 +4,56 @@ struct GapBuffer {
     size: u8,
     gap_left: u8,
     gap_right: u8,
-    buffer: [u8]
+    buffer: Vec<char>
 }
 
 impl GapBuffer {
+    // Controls the initialization of the array
+    fn from_size(size: u8) -> GapBuffer {
+        let buffer = Vec::with_capacity(size.into());
+
+        GapBuffer {
+            size: size,
+            gap_right: 0,
+            gap_left: size - 1,
+            buffer: buffer
+        }
+    }
     // Moves the gap left in the array
-    fn left(&self) {
+    fn left(&mut self) {
+        
     }
 
     // Moves the gap right in the array
-    fn right(&self) {
+    fn right(&mut self) {
     }
     
     // Controls the growth of the size of the buffer
-    fn grow(&self) {
+    fn grow(&mut self) {
     }
     
     // Controls the movement of the gap linked to the cursor insertion
-    fn move_cursor(&self) {
+    fn move_cursor(&mut self, pos: &u8) {
+        if *pos < self.gap_right {
+            self.right();
+        } else {
+            self.left()
+        }
     }
     
     // Controls data insertion operations
-    fn insert(&self) {
+    fn insert(&mut self, pos: u8, value: char) {
+        
+        if pos != self.gap_left {
+            self.move_cursor(&pos);
+        }
+
+        if self.gap_left == self.gap_right {
+            self.grow();
+        }
+        
+        self.buffer[pos as usize] = value;
+        self.gap_left += 1
+         
     }
 }
