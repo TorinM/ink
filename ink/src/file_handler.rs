d7f3a8b7a178ef22ac7394f465c50843fb7615a8
TorinM <torinmay@gmail.com>
2025-10-16 12:43:48 -0500

    Implemented file overwrite

--- ink/src/file_handler.rs
+++ ink/src/file_handler.rs
@@ -1,5 +1,7 @@
 use std::fs::File;
+use std::io::{Seek, SeekFrom, Write};
 
-pub fn overwrite_file(file: &mut File) {
-    todo!("Not implemented")
+pub fn overwrite_file(file: &mut File, data_buf: &Vec<u8>) -> Result<(), std::io::Error> {
+    file.seek(SeekFrom::Start(0)).unwrap();
+    file.write_all(data_buf)
 }
